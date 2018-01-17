3cb14d6085e8f78fc7a3bb8d8e7a283152ac0d2a
phanindra24 <phanindraiitb@gmail.com>
2018-01-17 14:37:42 +0530

    Working Tab Navigation

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MainContainer from './app/components/MainContainer/index'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainContainer />
+    );
+  }
+}
---
+++ app/components/MainContainer/index.js
@@ -0,0 +1,14 @@
+// import styles from './styles'
+import React from 'react';
+import HaystackContainer from "../HaystackContainer/index"
+
+class MainContainer extends React.Component {
+    
+
+    render() {
+        return <HaystackContainer />
+    }
+}
+
+
+module.exports = MainContainer;
